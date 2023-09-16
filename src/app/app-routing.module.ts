ba570a024a59cef8ae0556a4027ea0ca25fee1a9
pradpasu <pradyumnapasumarty1997@gmail.com>
2023-09-16 13:38:45 -0400

    added bootstrap styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomePageComponent,
+    path: "",
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
